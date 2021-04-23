77237a1c55375ef60b57954f73996ce490725b2e
eabeUO <eabe@uoregon.edu>
2021-04-23 08:55:25 -0700

    upsample instead of unpool

--- models/vae_3dmp.py
+++ models/vae_3dmp.py
@@ -81,10 +81,12 @@ class VAE3dmp(BaseVAE):
         hidden_dims.reverse()
         self.decoder = nn.ModuleList()
         for layer_n, i in enumerate(range(len(hidden_dims) - 1)):
-            self.decoder.add_module(str('maxunpool%i' % layer_n),
-                                    nn.MaxUnpool3d(kernel_size=2, #self.kernels[layer_n], 
-                                                stride=(self.tstrides[layer_n], self.xystrides[layer_n], self.xystrides[layer_n]), 
-                                                padding=0))
+            # self.decoder.add_module(str('maxunpool%i' % layer_n),
+            #                         nn.MaxUnpool3d(kernel_size=2, #self.kernels[layer_n], 
+            #                                     stride=(self.tstrides[layer_n], self.xystrides[layer_n], self.xystrides[layer_n]), 
+            #                                     padding=0))
+            self.decoder.add_module(str('upsample%i' % layer_n),nn.Upsample(scale_factor=2, mode='nearest'))
+
             self.decoder.add_module(str('convtranspose%i' % layer_n),
                                     nn.ConvTranspose3d(hidden_dims[i],
                                        hidden_dims[i + 1],
@@ -97,10 +99,11 @@ class VAE3dmp(BaseVAE):
 
         
         self.final_layer = nn.ModuleList()
-        self.final_layer.add_module(str('last_maxunpool%i' % 0),
-                                nn.MaxUnpool3d(kernel_size=2, #self.kernels[layer_n], 
-                                                stride=(self.tstrides[0], self.xystrides[0], self.xystrides[0]), 
-                                                padding=0))
+        # self.final_layer.add_module(str('last_maxunpool%i' % 0),
+        #                         nn.MaxUnpool3d(kernel_size=2, #self.kernels[layer_n], 
+        #                                         stride=(self.tstrides[0], self.xystrides[0], self.xystrides[0]), 
+        #                                         padding=0))
+        self.final_layer.add_module(str('last_upsample%i' % 0),nn.Upsample(scale_factor=2, mode='nearest'))
         self.final_layer.add_module(str('last_convtranspose%i' % 0),
                                 nn.ConvTranspose3d(hidden_dims[-1],
                                     hidden_dims[-1],
@@ -189,8 +192,9 @@ class VAE3dmp(BaseVAE):
 
     def grab_latents(self, inputs: Tensor, **kwargs) -> List[Tensor]:
         mu, log_var, pool_idx, target_output_size = self.encode(inputs)
+        pool_idx2, target_output_size2 = pool_idx.copy(), target_output_size.copy()
         z = self.reparameterize(mu, log_var)
-        return  [z.detach(), self.decode(pool_idx, target_output_size, z).detach(), pool_idx, target_output_size, inputs.detach(), mu.detach(), log_var.detach()]
+        return  [z.detach(), self.decode(pool_idx, target_output_size, z).detach(), pool_idx2, target_output_size2, inputs.detach(), mu.detach(), log_var.detach()]
 
     def loss_function(self,
                       *args,
